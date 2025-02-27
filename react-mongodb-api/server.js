7664510ec0256dbfc8c1a642ef83d16c7a6a0517
Josie <157422818+JosieWettlaufer@users.noreply.github.com>
2025-02-27 16:23:11 -0700

    Define schema before establishing db connection. Modify URLs to match collection name (recipepages)

--- react-mongodb-api/server.js
+++ react-mongodb-api/server.js
@@ -9,16 +9,7 @@ const PORT = process.env.PORT || 5000;  // Make sure this is NOT 3000
 
 //middleware to handle JSON data and CORS
 app.use(express.json());
-app.use(cors());
-
-//Connect to mongodb
-mongoose.connect('mongodb://localhost:27017/BakerBuddyDB', {})
-//test connection
-.then(() => {
-    console.log("Connected to MongoDB");
-    
-    return RecipePage.create({ userId: 1, title: "Chocolate Cake", timerList: "testtimer", unitConversions: "testUnit" });
-});
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 //Define simple schema for recipe
 const recipeSchema = new mongoose.Schema({
@@ -29,23 +20,34 @@ const recipeSchema = new mongoose.Schema({
 });
 
 //Create model based on schema
-const RecipePage = mongoose.model('RecipePage', recipeSchema);
+const RecipePage = mongoose.model('Recipepage', recipeSchema);
+
+//Connect to mongodb
+mongoose.connect('mongodb://localhost:27017/BakerBuddyDB', {})
+//test connection
+.then(() => {
+    console.log("Connected to MongoDB");
+    
+    return RecipePage.create({ userId: 1, title: "Chocolate Cake", timerList: "testtimer", unitConversions: "testUnit" });
+});
+
+
 
 //Define GET route to get all recipePages
-app.get('/api/recipePages', async (req, res) => {
+app.get('/api/recipepages', async (req, res) => {
     const recipePages = await RecipePage.find(); //pause fx until await code finishes
     res.json(recipePages); //send recipePages as JSON response
 });
 
 //define post route to add new entity
-app.post('/api/recipePages', async (req, res) => {
+app.post('/api/recipepages', async (req, res) => {
     const newRecipePage = new RecipePage(req.body); //gets studednt info from req body, stores in var
     await newRecipePage.save();
     res.json(newRecipePage);
 })
 
 //Define delete path
-app.delete('/api/recipePages/:id', async (req, res) => {
+app.delete('/api/recipepages/:id', async (req, res) => {
     await RecipePage.findByIdAndDelete(req.params.id); //access id from url
     res.json({ message: 'recipePage deleted' });
 });
