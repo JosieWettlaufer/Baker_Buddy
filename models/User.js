b8166690ef6d1da6dfa981b7a2330ca8418d5a8c
Josie <157422818+JosieWettlaufer@users.noreply.github.com>
2025-03-24 22:06:23 -0600

    began adding recipe page

--- models/User.js
+++ models/User.js
@@ -8,13 +8,25 @@ const timerSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now }
 });
 
+//Experimente
+const unitSchema = new mongoose.Schema({
+  category: { type: String, required: true }
+})
+
+//Experimente
+const recipePageSchema = new mongoose.Schema({
+  label: { type: String, required: true }, 
+  timers: [timerSchema],
+  unitConverters: [unitSchema]
+})
+
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: {type: String, required: true},
   password: { type: String, required: true },
-  timers: [timerSchema] // List of timers
-  
+  pages: [recipePageSchema]
+  //timers: [timerSchema] // List of timers [old code]
 });
 
 
